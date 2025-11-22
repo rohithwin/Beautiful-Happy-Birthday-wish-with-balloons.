4b8160d45d94fb73a2cf9563451285d73246a155
rohithwin <68723222+rohithwin@users.noreply.github.com>
2025-11-22 11:17:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-var c = document.getElementById("c");
-var ctx = c.getContext("2d");
-
 var w = c.width = window.innerWidth,
     h = c.height = window.innerHeight,
+    ctx = c.getContext('2d'),
 
-    hw = w / 2,
+    hw = w / 2, // half-width
     hh = h / 2,
 
     opts = {
@@ -12,6 +10,9 @@ var w = c.width = window.innerWidth,
         charSize: 30,
         charSpacing: 35,
         lineHeight: 40,
+
+        cx: w / 2,
+        cy: h / 2,
 
         fireworkPrevPoints: 10,
         fireworkBaseLineWidth: 5,
@@ -32,11 +33,9 @@ var w = c.width = window.innerWidth,
         fireworkShardAddedVel: 2,
         fireworkShardBaseSize: 3,
         fireworkShardAddedSize: 3,
-
         gravity: .1,
         upFlow: -.1,
         letterContemplatingWaitTime: 360,
-
         balloonSpawnTime: 20,
         balloonBaseInflateTime: 10,
         balloonAddedInflateTime: 10,
@@ -47,13 +46,17 @@ var w = c.width = window.innerWidth,
         balloonBaseRadian: -(Math.PI / 2 - .5),
         balloonAddedRadian: -1,
     },
+    calc = {
+        totalWidth: opts.charSpacing *
+            Math.max(opts.strings[0].length, opts.strings[1].length)
+    },
 
     Tau = Math.PI * 2,
     TauQuarter = Tau / 4,
 
     letters = [];
 
-ctx.font = opts.charSize + "px Verdana";
+ctx.font = opts.charSize + 'px Verdana';
 
 function Letter(char, x, y) {
     this.char = char;
@@ -61,21 +64,21 @@ function Letter(char, x, y) {
     this.y = y;
 
     this.dx = -ctx.measureText(char).width / 2;
-    this.dy = opts.charSize / 2;
+    this.dy = +opts.charSize / 2;
 
     this.fireworkDy = this.y - hh;
 
-    var hue = (x + hw) / w * 360;
+    var hue = x / calc.totalWidth * 360;
 
-    this.color = `hsl(${hue},80%,50%)`;
-    this.lightAlphaColor = `hsla(${hue},80%,50%,alp)`;
-    this.lightColor = `hsl(${hue},80%,70%)`;
-    this.alphaColor = `hsla(${hue},80%,50%,alp)`;
+    this.color = 'hsl(hue,80%,50%)'.replace('hue', hue);
+    this.lightAlphaColor = 'hsla(hue,80%,light%,alp)'.replace('hue', hue);
+    this.lightColor = 'hsl(hue,80%,light%)'.replace('hue', hue);
+    this.alphaColor = 'hsla(hue,80%,50%,alp)'.replace('hue', hue);
 
     this.reset();
 }
-
 Letter.prototype.reset = function () {
+
     this.phase = 'firework';
     this.tick = 0;
     this.spawned = false;
@@ -84,25 +87,27 @@ Letter.prototype.reset = function () {
     this.lineWidth = opts.fireworkBaseLineWidth + opts.fireworkAddedLineWidth * Math.random();
     this.prevPoints = [[0, hh, 0]];
 }
-
 Letter.prototype.step = function () {
 
     if (this.phase === 'firework') {
 
         if (!this.spawned) {
+
             ++this.tick;
             if (this.tick >= this.spawningTime) {
+
                 this.tick = 0;
                 this.spawned = true;
             }
 
         } else {
+
             ++this.tick;
 
             var linearProportion = this.tick / this.reachTime,
                 armonicProportion = Math.sin(linearProportion * TauQuarter),
 
-                x = linearProportion * (this.x + hw),
+                x = linearProportion * this.x,
                 y = hh + armonicProportion * this.fireworkDy;
 
             if (this.prevPoints.length > opts.fireworkPrevPoints)
@@ -110,51 +115,59 @@ Letter.prototype.step = function () {
 
             this.prevPoints.push([x, y, linearProportion * this.lineWidth]);
 
+            var lineWidthProportion = 1 / (this.prevPoints.length - 1);
+
             for (var i = 1; i < this.prevPoints.length; ++i) {
 
-                var p = this.prevPoints[i],
-                    p2 = this.prevPoints[i - 1];
+                var point = this.prevPoints[i],
+                    point2 = this.prevPoints[i - 1];
 
                 ctx.strokeStyle = this.alphaColor.replace('alp', i / this.prevPoints.length);
-                ctx.lineWidth = p[2] * (i / this.prevPoints.length);
+                ctx.lineWidth = point[2] * lineWidthProportion * i;
                 ctx.beginPath();
-                ctx.moveTo(p[0], p[1]);
-                ctx.lineTo(p2[0], p2[1]);
+                ctx.moveTo(point[0], point[1]);
+                ctx.lineTo(point2[0], point2[1]);
                 ctx.stroke();
+
             }
 
             if (this.tick >= this.reachTime) {
 
                 this.phase = 'contemplate';
-                this.circleFinalSize = opts.fireworkCircleBaseSize + opts.fireworkCircleAddedSize * Math.random();
-                this.circleCompleteTime = opts.fireworkCircleBaseTime + opts.fireworkCircleAddedTime * Math.random() | 0;
 
+                this.circleFinalSize = opts.fireworkCircleBaseSize +
+                    opts.fireworkCircleAddedSize * Math.random();
+                this.circleCompleteTime = opts.fireworkCircleBaseTime +
+                    opts.fireworkCircleAddedTime * Math.random() | 0;
                 this.circleCreating = true;
                 this.circleFading = false;
 
-                this.circleFadeTime = opts.fireworkCircleFadeBaseTime + opts.fireworkCircleFadeAddedTime * Math.random() | 0;
+                this.circleFadeTime = opts.fireworkCircleFadeBaseTime +
+                    opts.fireworkCircleFadeAddedTime * Math.random() | 0;
                 this.tick = 0;
                 this.tick2 = 0;
 
                 this.shards = [];
 
-                var count = opts.fireworkBaseShards + opts.fireworkAddedShards * Math.random() | 0,
-                    angle = Tau / count,
+                var shardCount = opts.fireworkBaseShards +
+                    opts.fireworkAddedShards * Math.random() | 0,
+                    angle = Tau / shardCount,
                     cos = Math.cos(angle),
                     sin = Math.sin(angle),
-                    px = 1,
-                    py = 0;
 
-                for (var i = 0; i < count; ++i) {
-                    let px2 = px;
-                    px = px * cos - py * sin;
-                    py = py * cos + px2 * sin;
+                    x = 1,
+                    y = 0;
 
-                    this.shards.push(new Shard(this.x, this.y, px, py, this.alphaColor));
+                for (var i = 0; i < shardCount; ++i) {
+                    var x1 = x;
+                    x = x * cos - y * sin;
+                    y = y * cos + x1 * sin;
+
+                    this.shards.push(new Shard(this.x, this.y, x, y, this.alphaColor));
                 }
             }
-        }
 
+        }
     } else if (this.phase === 'contemplate') {
 
         ++this.tick;
@@ -162,32 +175,32 @@ Letter.prototype.step = function () {
         if (this.circleCreating) {
 
             ++this.tick2;
-
-            var prop = this.tick2 / this.circleCompleteTime,
-                armonic = -Math.cos(prop * Math.PI) / 2 + .5;
+            var proportion = this.tick2 / this.circleCompleteTime,
+                armonic = -Math.cos(proportion * Math.PI) / 2 + .5;
 
             ctx.beginPath();
-            ctx.fillStyle = this.lightAlphaColor.replace('alp', prop);
+            ctx.fillStyle = this.lightAlphaColor.replace('light',
+                50 + 50 * proportion).replace('alp', proportion);
+            ctx.beginPath();
             ctx.arc(this.x, this.y, armonic * this.circleFinalSize, 0, Tau);
             ctx.fill();
 
-            if (this.tick2 >= this.circleCompleteTime) {
+            if (this.tick2 > this.circleCompleteTime) {
                 this.tick2 = 0;
                 this.circleCreating = false;
                 this.circleFading = true;
             }
-
         } else if (this.circleFading) {
 
-            ctx.fillStyle = this.lightColor;
+            ctx.fillStyle = this.lightColor.replace('light', 70);
             ctx.fillText(this.char, this.x + this.dx, this.y + this.dy);
 
             ++this.tick2;
-            var prop = this.tick2 / this.circleFadeTime,
-                armonic = -Math.cos(prop * Math.PI) / 2 + .5;
+            var proportion = this.tick2 / this.circleFadeTime,
+                armonic = -Math.cos(proportion * Math.PI) / 2 + .5;
 
             ctx.beginPath();
-            ctx.fillStyle = this.lightAlphaColor.replace('alp', 1 - armonic);
+            ctx.fillStyle = this.lightAlphaColor.replace('light', 100).replace('alp', 1 - armonic);
             ctx.arc(this.x, this.y, this.circleFinalSize, 0, Tau);
             ctx.fill();
 
@@ -195,12 +208,15 @@ Letter.prototype.step = function () {
                 this.circleFading = false;
 
         } else {
-            ctx.fillStyle = this.lightColor;
+
+            ctx.fillStyle = this.lightColor.replace('light', 70);
             ctx.fillText(this.char, this.x + this.dx, this.y + this.dy);
         }
 
         for (var i = 0; i < this.shards.length; ++i) {
+
             this.shards[i].step();
+
             if (!this.shards[i].alive) {
                 this.shards.splice(i, 1);
                 --i;
@@ -209,32 +225,29 @@ Letter.prototype.step = function () {
 
         if (this.tick > opts.letterContemplatingWaitTime) {
 
-            this.phase = "balloon";
-            this.tick = 0;
+            this.phase = 'balloon';
 
+            this.tick = 0;
             this.spawning = true;
             this.spawnTime = opts.balloonSpawnTime * Math.random() | 0;
-
             this.inflating = false;
             this.inflateTime = opts.balloonBaseInflateTime + opts.balloonAddedInflateTime * Math.random() | 0;
-
             this.size = opts.balloonBaseSize + opts.balloonAddedSize * Math.random() | 0;
 
-            var rad = opts.balloonBaseRadian + opts.balloonAddedRadian * Math.random();
-            var vel = opts.balloonBaseVel + opts.balloonAddedVel * Math.random();
+            var rad = opts.balloonBaseRadian + opts.balloonAddedRadian * Math.random(),
+                vel = opts.balloonBaseVel + opts.balloonAddedVel * Math.random();
 
             this.vx = Math.cos(rad) * vel;
             this.vy = Math.sin(rad) * vel;
         }
-
     } else if (this.phase === 'balloon') {
 
-        ctx.strokeStyle = this.lightColor;
+        ctx.strokeStyle = this.lightColor.replace('light', 80);
 
         if (this.spawning) {
 
             ++this.tick;
-            ctx.fillStyle = this.lightColor;
+            ctx.fillStyle = this.lightColor.replace('light', 70);
             ctx.fillText(this.char, this.x + this.dx, this.y + this.dy);
 
             if (this.tick >= this.spawnTime) {
@@ -242,18 +255,17 @@ Letter.prototype.step = function () {
                 this.spawning = false;
                 this.inflating = true;
             }
-
         } else if (this.inflating) {
 
             ++this.tick;
 
-            var prop = this.tick / this.inflateTime,
+            var proportion = this.tick / this.inflateTime,
                 x = this.cx = this.x,
-                y = this.cy = this.y - this.size * prop;
+                y = this.cy = this.y - this.size * proportion;
 
-            ctx.fillStyle = this.alphaColor.replace('alp', prop);
+            ctx.fillStyle = this.alphaColor.replace('alp', proportion);
             ctx.beginPath();
-            generateBalloonPath(x, y, this.size * prop);
+            generateBalloonPath(x, y, this.size * proportion);
             ctx.fill();
 
             ctx.beginPath();
@@ -261,7 +273,7 @@ Letter.prototype.step = function () {
             ctx.lineTo(x, this.y);
             ctx.stroke();
 
-            ctx.fillStyle = this.lightColor;
+            ctx.fillStyle = this.lightColor.replace('light', 70);
             ctx.fillText(this.char, this.x + this.dx, this.y + this.dy);
 
             if (this.tick >= this.inflateTime) {
@@ -284,18 +296,20 @@ Letter.prototype.step = function () {
             ctx.lineTo(this.cx, this.cy + this.size);
             ctx.stroke();
 
-            ctx.fillStyle = this.lightColor;
+            ctx.fillStyle = this.lightColor.replace('light', 70);
             ctx.fillText(this.char, this.cx + this.dx, this.cy + this.dy + this.size);
 
-            if (this.cy + this.size < -hh)
-                this.phase = "done";
+            if (this.cy + this.size < -hh || this.cx < -hw || this.cy > hw)
+                this.phase = 'done';
+
         }
     }
 }
 
 function Shard(x, y, vx, vy, color) {
 
-    var vel = opts.fireworkShardBaseVel + opts.fireworkShardAddedVel * Math.random();
+    var vel = opts.fireworkShardBaseVel +
+        opts.fireworkShardAddedVel * Math.random();
 
     this.vx = vx * vel;
     this.vy = vy * vel;
@@ -307,9 +321,10 @@ function Shard(x, y, vx, vy, color) {
     this.color = color;
 
     this.alive = true;
-    this.size = opts.fireworkShardBaseSize + opts.fireworkShardAddedSize * Math.random();
-}
 
+    this.size = opts.fireworkShardBaseSize +
+        opts.fireworkShardAddedSize * Math.random();
+}
 Shard.prototype.step = function () {
 
     this.x += this.vx;
@@ -320,52 +335,51 @@ Shard.prototype.step = function () {
 
     this.prevPoints.push([this.x, this.y]);
 
-    for (var i = 1; i < this.prevPoints.length; ++i) {
+    var lineWidthProportion = this.size / this.prevPoints.length;
 
-        var p = this.prevPoints[i],
-            p2 = this.prevPoints[i - 1];
+    for (var k = 0; k < this.prevPoints.length - 1; ++k) {
 
-        ctx.strokeStyle = this.color.replace('alp', i / this.prevPoints.length);
-        ctx.lineWidth = i / this.prevPoints.length * this.size;
+        var point = this.prevPoints[k],
+            point2 = this.prevPoints[k + 1];
+
+        ctx.strokeStyle = this.color.replace('alp', k / this.prevPoints.length);
+        ctx.lineWidth = k * lineWidthProportion;
         ctx.beginPath();
-        ctx.moveTo(p[0], p[1]);
-        ctx.lineTo(p2[0], p2[1]);
+        ctx.moveTo(point[0], point[1]);
+        ctx.lineTo(point2[0], point2[1]);
         ctx.stroke();
+
     }
 
-    if (this.y > h || this.x < -hw || this.x > hw)
+    if (this.prevPoints[0][1] > hh)
         this.alive = false;
 }
 
 function generateBalloonPath(x, y, size) {
 
     ctx.moveTo(x, y);
-    ctx.bezierCurveTo(
-        x - size / 2, y - size / 2,
+    ctx.bezierCurveTo(x - size / 2, y - size / 2,
         x - size / 4, y - size,
-        x, y - size
-    );
-
-    ctx.bezierCurveTo(
-        x + size / 4, y - size,
+        x, y - size);
+    ctx.bezierCurveTo(x + size / 4, y - size,
         x + size / 2, y - size / 2,
-        x, y
-    );
+        x, y);
 }
 
 function anim() {
 
-    requestAnimationFrame(anim);
+    window.requestAnimationFrame(anim);
 
-    ctx.fillStyle = "#111";
+    ctx.fillStyle = '#111';
     ctx.fillRect(0, 0, w, h);
 
     ctx.translate(hw, hh);
 
     var done = true;
     for (var l = 0; l < letters.length; ++l) {
+
         letters[l].step();
-        if (letters[l].phase !== "done")
+        if (letters[l].phase !== 'done')
             done = false;
     }
 
@@ -376,15 +390,16 @@ function anim() {
             letters[l].reset();
 }
 
-for (let i = 0; i < opts.strings.length; i++) {
-    let line = opts.strings[i];
-    let totalWidth = line.length * opts.charSpacing;
-
-    for (let j = 0; j < line.length; j++) {
+for (var i = 0; i < opts.strings.length; ++i) {
+    for (var j = 0; j < opts.strings[i].length; ++j) {
         letters.push(new Letter(
-            line[j],
-            j * opts.charSpacing - totalWidth / 2,
-            i * opts.lineHeight - (opts.strings.length * opts.lineHeight) / 2
+            opts.strings[i][j],
+            j * opts.charSpacing +
+            opts.charSpacing / 2 -
+            opts.strings[i].length * opts.charSize / 2,
+            i * opts.lineHeight +
+            opts.lineHeight / 2 -
+            opts.strings.length * opts.lineHeight / 2
         ));
     }
 }
